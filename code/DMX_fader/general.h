14edd1477faa7b3f448257f2821add7ae699d531
Øystein Smith <oystein.smith89@gmail.com>
2020-10-26 22:06:29 +0100

    defines for reading twi address from dip switch

--- code/DMX_fader/general.h
+++ code/DMX_fader/general.h
@@ -12,6 +12,11 @@
 //#define MASTER
 #define SLAVE
 
-
+#define ADDRESS_PORT PIND
+#define A0 PD5
+#define A1 PD6
+#define A2 PD7
+#define ADDRESS_MASK ( (1 << A0) | (1 << A1) | (1 << A2) )
+#define ADDRESS_OFFSET A0
 
 #endif /* GENERAL_H_ */
